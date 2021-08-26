d281f425473f8e1d3a0f563b8d4052f15961fe91
Kazuho-Shibuya <businessfive25@gmail.com>
2021-08-26 14:02:05 +0900

    moduleで名前空間を追加

--- hello.rb
+++ hello.rb
@@ -93,3 +93,21 @@ end
 tom = AdminUser.new('tom')
 tom.sayHi
 tom.sayHello
+
+# module
+# - 名前空間
+
+module Movie
+  VERSION = 1.1
+  def self.encode
+    puts 'encoding...'
+  end
+
+  def self.export
+    puts 'exporting...'
+  end
+end
+
+Movie.encode
+Movie.export
+p Movie::VERSION
